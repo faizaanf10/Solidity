b29532f0337a93d06dafb43af63adff37ca031ed
Faizaan Farooqui <71724231+faizaanf10@users.noreply.github.com>
2022-11-15 22:14:56 +0530

    Update BT3.sol

--- BT3.sol
+++ BT3.sol
@@ -1,21 +1,26 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.7;
+pragma solidity >= 0.5.0 < 0.9.0;
 
-contract Bank {
-   address public owner;
-   uint public balance;
-   
-   constructor() {
-     owner = msg.sender;
-   }
-   
-   function deposit() external payable {
-     balance += msg.value;
-   }
-   
-   function withdraw(uint _amount) external {     
-     require(balance >= _amount, "Insufficient balance!");
-     payable(owner).transfer(_amount);
-     balance -= _amount;
-   }
+contract Wallet{
+
+    mapping(address => uint) balance;
+    address owner;
+
+    constructor(){
+        owner = payable(msg.sender); //owner ki value kardo jisne transaction initiate kiya but payable
+    }
+
+    function deposit() public payable{
+        balance[msg.sender] = msg.value;  //msg.value is amount of ether sent in transaction
+    }
+
+    function withdraw(uint _amount) public payable{
+        require(balance[msg.sender] >= _amount, "amount should be less than available balance");
+        payable(msg.sender).transfer(_amount);
+        balance[msg.sender] = balance[msg.sender] - _amount;
+    }
+
+    function balanceOf() public view returns(uint){
+        return balance[msg.sender];
+    }
 }
